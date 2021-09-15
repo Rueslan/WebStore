f0b2c1be762a55815d631eba34531857e0221853
Rueslan <krus1501@mail.ru>
2021-09-15 14:03:05 +0400

    Добавлено новое свойство "Выслуга лет"

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebStore.Models
+﻿namespace WebStore.Models
 {
     public class Employee
     {
@@ -12,5 +7,6 @@ namespace WebStore.Models
         public string LastName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
+        public int Seniority { get; set; }
     }
 }
