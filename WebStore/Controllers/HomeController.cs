8a89e6a80ac36af5154732e5cc26e1f817a312ee
Rueslan <krus1501@mail.ru>
2021-09-15 12:35:11 +0400

    Представление сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29 },
+            new Employee { id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35 },
+            new Employee { id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44 }
+        };
+
         public IActionResult Index()
         {
             return View(); 
@@ -11,6 +20,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"Second Action wwith parametr {id}");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
         }
     }
 }
