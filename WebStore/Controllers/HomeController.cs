bff4fd55fdbeae26dbbc6812c693c77cb156e951
Rueslan <krus1501@mail.ru>
2021-09-15 12:05:02 +0400

    Первый контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello from first controller");
+        }
+        public IActionResult SecondAction(string id)
+        {
+            return Content($"Second Action wwith parametr {id}");
+        }
+    }
+}
