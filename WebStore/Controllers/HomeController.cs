4ab5cabf6ea89963d5352681cc593972dc731085
Rueslan <krus1501@mail.ru>
2021-09-15 14:06:51 +0400

    В контроллер добавлено новое действие, открывающее представление о выбранном по id сотруднике

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace WebStore.Controllers
     {
         private static readonly List<Employee> _Employees = new()
         {
-            new Employee { id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29 },
-            new Employee { id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35 },
-            new Employee { id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44 }
+            new Employee { id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
+            new Employee { id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
+            new Employee { id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44, Seniority = 6 }
         };
 
         public IActionResult Index()
@@ -25,6 +25,11 @@ namespace WebStore.Controllers
         public IActionResult Employees()
         {
             return View(_Employees);
+        }
+
+        public IActionResult CurrentEmployee(int currentId)
+        {
+            return View("CurrentEmployee", _Employees[currentId - 1]);
         }
     }
 }
