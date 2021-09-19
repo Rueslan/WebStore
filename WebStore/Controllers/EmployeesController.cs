3327bdfedbf0ce3c5b4bcba214361ec3545301f6
Rueslan <krus1501@mail.ru>
2021-09-19 11:46:59 +0400

    Выделение работы с сотрудниками в отдельный контроллер

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
+            new Employee { id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
+            new Employee { id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44, Seniority = 6 }
+        };
+
+        public IActionResult GetIndex()
+        {
+            return View(_Employees);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,35 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> _Employees = new()
+        public IActionResult GetIndex()
         {
-            new Employee { id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
-            new Employee { id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
-            new Employee { id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44, Seniority = 6 }
-        };
-
-        public IActionResult Index()
-        {
-            return View(); 
-        }
-        public IActionResult SecondAction(string id)
-        {
-            return Content($"Second Action wwith parametr {id}");
+            return View();
         }
 
-        public IActionResult Employees()
-        {
-            return View(_Employees);
-        }
-
-        public IActionResult CurrentEmployee(int currentId)
-        {
-            return View("CurrentEmployee", _Employees[currentId - 1]);
-        }
+        //public IActionResult CurrentEmployee(int currentId)
+        // {
+        //     return View("CurrentEmployee", _Employees[currentId - 1]);
+        //}
     }
 }
